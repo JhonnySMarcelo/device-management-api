f832395f982c29ce012e9674703ab97710d1a545
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2026-01-30 00:18:40 -0300

    feat: add device domain and device state enum

---
+++ DeviceManagement.Api/Domain/Device.cs
@@ -0,0 +1,11 @@
+namespace DeviceManagement.Api.Domain
+{
+    public class Device
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public DeviceState State { get; set; } = DeviceState.Available;
+        public DateTime CreationTime { get; init; } = DateTime.UtcNow;
+    }
+}
---
+++ DeviceManagement.Api/Domain/DeviceState.cs
@@ -0,0 +1,9 @@
+﻿namespace DeviceManagement.Api.Domain
+{
+    public enum DeviceState
+    {
+        Available = 0,
+        InUse = 1, 
+        Inactive = 2
+    }
+}
