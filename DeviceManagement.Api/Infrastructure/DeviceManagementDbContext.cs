625a66519573aa1f224a11df391b7924f3444c80
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2026-01-31 03:08:48 -0300

    chore: setup Entity Framework Core infrastructure
    - add application DbContext to centralize data access
    - configure EF Core with SQL Server for relational persistence
    - define connection string via configuration for environment flexibility
    - register DbContext in DI to enable scoped access across the app

---
+++ DeviceManagement.Api/Infrastructure/DeviceManagementDbContext.cs
@@ -0,0 +1,41 @@
+﻿using DeviceManagement.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeviceManagement.Api.Infrastructure
+{
+    public class DeviceManagementDbContext : DbContext
+    {
+        public DeviceManagementDbContext(
+            DbContextOptions<DeviceManagementDbContext> options
+            ) : base (options) { }
+
+        public DbSet<Device> Devices => Set<Device>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.ToTable("Devices");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(d => d.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(d => d.Brand)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(d => d.State)
+                      .IsRequired()
+                      .HasConversion<int>();
+
+                entity.Property(d => d.CreationTime)
+                      .IsRequired();
+            });
+        }
+    }
+}
--- DeviceManagement.Api/Program.cs
+++ DeviceManagement.Api/Program.cs
@@ -1,3 +1,6 @@
+using DeviceManagement.Api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DeviceManagementDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DeviceManagementDatabase"));
+});
 
 var app = builder.Build();
 
